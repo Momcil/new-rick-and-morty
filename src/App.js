32ff70ccd6aa49c55731c50989e718b7b1f82cdd
momchil <momchil@momchils-MacBook-Air.local>
2022-04-05 15:39:25 +0200

    add header navbar and card

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <h1>Rick and Morty-App</h1>
       </header>
+      <div className='Card'>
+        <img src='https://rickandmortyapi.com/api/character/avatar/2.jpeg'></img>
+        <h2>Morty Smith</h2>
+        <button>show more</button>
+      </div>
+      <div className='Navbar'>
+        <ul>
+          <li ></li>
+          <li className='Home/Dashboard'><a href='#'>Dashboard</a></li>
+          <li className='Ranrom Characters'><a href='#'>Ranrom</a></li>
+          <li className='Favorites'><a href='#'>Favorites</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
